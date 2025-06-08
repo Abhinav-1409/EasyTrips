1493efb95e1a181362e3e851def06bd659635816
Atul <atulanand05241@gmail.com>
2025-06-08 14:46:54 +0530

    destinationmodel added

---
+++ backend/models/destination.js
@@ -0,0 +1,135 @@
+const { group } = require("console");
+const mongoose = require("mongoose");
+
+const destinationSchema = new mongoose.Schema({
+   name: {
+      type: String,
+      required: true,
+      trim: true,      
+   },
+   description: {
+      type: String,
+      required: true,
+      trim: true,
+   },
+   highlights: [{
+      type: String,
+      required: true,
+      trim: true,
+   }],
+   bestTimeToVisit: {
+      type: String,
+      required: true,
+      trim: true,
+   },
+   groupSize: {
+      type: String,
+      required: true,
+      trim: true,
+   },
+   duration: {
+      type: String,
+      required: true,
+      trim: true,
+   },
+   reviews: [{
+      userId: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: 'User',
+         required: true,
+      },
+      rating: {
+         type: Number,
+         required: true,
+         min: 1,
+         max: 5,
+      },
+      comment: {
+         type: String,
+         required: true,
+         trim: true,
+      },
+      createdAt: {
+         type: Date,
+         default: Date.now,
+      },
+   }],
+   ratings: {
+      average: {
+         type: Number,
+         default: 0,
+         min: 0,
+         max: 5,
+      },
+      count: {
+         type: Number,
+         default: 0,
+      },
+   },
+   location: {
+      type: String,
+      required: true,
+      trim: true,
+   },
+   imageUrl: [ {
+      type: String,
+      required: true,
+      trim: true,
+   }],
+   price: {
+      type: Number,
+      required: true,
+      min: 0,
+   },
+   availability: {
+      type: Boolean,
+      default: true,
+   },
+   itinerary: [{
+      day: {
+         type: Number,
+         required: true,
+      },
+      activities: [{
+         type: String,
+         required: true,
+         trim: true,
+      }],
+   }],
+   Whatincluded: [{
+      type: String,
+      required: true,
+      trim: true,
+   }],
+   Whatnotincluded: [{
+      type: String,
+      required: true,
+      trim: true,
+   }],
+   termsAndConditions: {
+      type: String,
+      required: true,
+      trim: true,
+   },
+   cancellationPolicy: {
+      type: String,
+      required: true,
+      trim: true,
+   },
+   tourOperator: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'TourOperator',
+      required: true,
+   },
+   createdAt: {
+      type: Date,
+      default: Date.now,
+   },
+   updatedAt: {
+      type: Date,
+      default: Date.now,
+   },
+});   
+
+const Destination = mongoose.model("Destination", destinationSchema);
+module.exports = Destination;
